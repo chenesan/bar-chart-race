2bc7ba3f56be9cb1c46dfb559879284d4286d4b7
陳乙山 <pipio1994@gmail.com>
2019-12-04 11:27:53 +0800

    feat: draw basic bars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { browserUsage } from '@vx/mock-data';
+import { Bar } from '@vx/shape';
+import { scaleLinear, scaleBand } from '@vx/scale';
+
+const data = browserUsage[0];
+const keys = [
+  'Google Chrome',
+  'Internet Explorer',
+  'Firefox',
+  'Safari',
+  'Microsoft Edge',
+  'Opera',
+  'Mozilla',
+  'Other/Unknown',
+]
+const numbers = keys.map(key => Number(data[key]));
+
+const width = 600;
+const height = 900;
+
+const xScale = scaleLinear({
+  domain: [0, Math.max(...numbers)],
+  range: [0, width],
+})
+const yScale = scaleBand({
+  domain: keys,
+  range: [0, height],
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <svg width={width} height={height}>
+        {numbers.map(
+          (value, idx) => {
+            const name = keys[idx];
+            const barX = 0;
+            const barY = yScale(name);
+            const barWidth = xScale(value);
+            const barHeight = yScale.bandwidth(); 
+            return <Bar
+              key={name}
+              x={barX}
+              y={barY}
+              width={barWidth}
+              height={barHeight}
+              fill={'blue'}
+            />
+          }
+        )}
+      </svg>
     </div>
   );
 }
