b2b54e6494f953ff94bbc3c14223b276e24dcfd8
陳乙山 <pipio1994@gmail.com>
2019-12-12 10:02:05 +0800

    Fetch data in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import csvParse from 'csv-parse/lib/sync';
 import BarChartAnimation from './BarChartAnimation';
 
+const dataUrl = '/data/category-brands.csv';
+
 function App() {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    axios.get(dataUrl).then(resp => {
+      const { data: csvString } = resp;
+      const nextData = csvParse(csvString).slice(1).map(
+        ([date, name, category, value]) => ({ date: new Date(date), name, category, value })
+      );
+      setData(nextData);
+    });
+  }, []);
   return (
-    <BarChartAnimation data={[]} />
+    <BarChartAnimation data={data} />
   );
 }
 
