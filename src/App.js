9f09435892865808cd149986c88e55f8f00233a8
陳乙山 <pipio1994@gmail.com>
2019-12-29 15:04:48 +0800

    Move makeKeyframes outside of BarChartAnimation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import BarChartAnimation from "./BarChartAnimation";
 
 const dataUrl = "/data/category-brands.csv";
 const numOfBars = 12;
-const duration = 250; 
 const numOfSlice = 10;
 const chartWidth = 1200;
 const chartHeight = 600;
@@ -16,8 +15,91 @@ const chartPadding = {
   left: 50
 };
 
+const buildFindData = data => {
+  const dataByDateAndName = new Map();
+  data.forEach(dataPoint => {
+    const { date, name } = dataPoint;
+    if (!dataByDateAndName.get(date)) {
+      dataByDateAndName.set(date, { [name]: dataPoint });
+    } else {
+      const nextGroup = {
+        ...dataByDateAndName.get(date),
+        [name]: dataPoint
+      };
+      dataByDateAndName.set(date, nextGroup);
+    }
+  });
+  const finder = ({ date, name }) => {
+    try {
+      return dataByDateAndName.get(date)[name];
+    } catch (e) {
+      return null;
+    }
+  };
+  return finder;
+}
+
+const makeKeyframes = (data, numOfSlice) => {
+  const findData = buildFindData(data);
+  const nameSet = new Set(data.map(({ name }) => name));
+  const nameList = [...nameSet];
+  const dateSet = new Set(data.map(({ date }) => date));
+  const dateList = [...dateSet];
+
+  const frames = dateList.sort().map(date => ({
+    date,
+    data: nameList.map(name => {
+      const dataPoint = findData({ date, name });
+      return {
+        ...dataPoint,
+        value: dataPoint ? dataPoint.value : 0,
+      };
+    })
+  }));
+  const keyframes = frames
+    .reduce((result, frame, idx) => {
+      const prev = frame;
+      const next = idx !== frames.length - 1 ? frames[idx + 1] : null;
+      if (!next) {
+        result.push({ ...frame, date: new Date(frame.date) });
+      } else {
+        const prevTimestamp = new Date(prev.date).getTime();
+        const nextTimestamp = new Date(next.date).getTime();
+        const diff = nextTimestamp - prevTimestamp;
+        for (let i = 0; i < numOfSlice; i++) {
+          const sliceDate = new Date(prevTimestamp + diff * i / numOfSlice);
+          const sliceData = frame.data.map(({ name, value, ...others }) => {
+            const prevValue = value;
+            const nextDataPoint = findData({ date: next.date, name });
+            const nextValue = nextDataPoint ? nextDataPoint.value : 0;
+            const sliceValue =
+              prevValue + (nextValue - prevValue) * i / numOfSlice;
+            return {
+              name,
+              value: sliceValue,
+              ...others
+            };
+          });
+          result.push({
+            date: sliceDate,
+            data: sliceData
+          });
+        }
+      }
+      return result;
+    }, [])
+    .map(({ date, data }) => {
+      return {
+        date,
+        data: data.sort((a, b) => b.value - a.value)
+      };
+    });
+
+  return keyframes;
+};
+
 function App() {
-  const [data, setData] = React.useState([]);
+  const [keyframes, setKeyframes] = React.useState([]);
   React.useEffect(() => {
     axios.get(dataUrl).then(resp => {
       const { data: csvString } = resp;
@@ -29,17 +111,16 @@ function App() {
           category,
           value: Number(value)
         }));
-      setData(nextData);
+      const keyframes = makeKeyframes(nextData, numOfSlice);
+      setKeyframes(keyframes);
     });
   }, []);
-  if (data.length === 0) {
+  if (keyframes.length === 0) {
     return false;
   }
   return <BarChartAnimation
-    data={data}
+    keyframes={keyframes}
     numOfBars={numOfBars}
-    numOfSlice={numOfSlice}
-    duration={duration}
     width={chartWidth}
     height={chartHeight}
     padding={chartPadding}
--- src/BarChartAnimation.js
+++ src/BarChartAnimation.js
@@ -25,101 +25,21 @@ class MyAxisTop extends React.Component {
 
 const AnimatedAxisTop = animated(MyAxisTop);
 
-const buildFindData = data => {
-  const dataByDateAndName = new Map();
-  data.forEach(dataPoint => {
-    const { date, name } = dataPoint;
-    if (!dataByDateAndName.get(date)) {
-      dataByDateAndName.set(date, { [name]: dataPoint });
-    } else {
-      const nextGroup = {
-        ...dataByDateAndName.get(date),
-        [name]: dataPoint
-      };
-      dataByDateAndName.set(date, nextGroup);
-    }
-  });
-  const finder = ({ date, name }) => {
-    try {
-      return dataByDateAndName.get(date)[name];
-    } catch (e) {
-      return null; 
-    }
-  };
-  return finder;
-}
-
-const makeKeyFrames = (data, numOfSlice) => {
-  const findData = buildFindData(data);
-  const nameSet = new Set(data.map(({ name }) => name));
-  const nameList = [...nameSet];
-  const dateSet = new Set(data.map(({ date }) => date));
-  const dateList = [...dateSet];
-
-  const frames = dateList.sort().map(date => ({
-    date,
-    data: nameList.map(name => {
-      const dataPoint = findData({ date , name});
-      return {
-        ...dataPoint,
-        value: dataPoint ? dataPoint.value : 0,
-      };
-    })
-  }));
-  const keyframes = frames
-    .reduce((result, frame, idx) => {
-      const prev = frame;
-      const next = idx !== frames.length - 1 ? frames[idx + 1] : null;
-      if (!next) {
-        result.push({ ...frame, date: new Date(frame.date) });
-      } else {
-        const prevTimestamp = new Date(prev.date).getTime();
-        const nextTimestamp = new Date(next.date).getTime();
-        const diff = nextTimestamp - prevTimestamp;
-        for (let i = 0; i < numOfSlice; i++) {
-          const sliceDate = new Date(prevTimestamp + diff * i / numOfSlice);
-          const sliceData = frame.data.map(({ name, value, ...others }) => {
-            const prevValue = value;
-            const nextDataPoint = findData({ date: next.date, name });
-            const nextValue = nextDataPoint ? nextDataPoint.value : 0;
-            const sliceValue =
-              prevValue + (nextValue - prevValue) * i / numOfSlice;
-            return {
-              name,
-              value: sliceValue,
-              ...others
-            };
-          });
-          result.push({
-            date: sliceDate,
-            data: sliceData
-          });
-        }
-      }
-      return result;
-    }, [])
-    .map(({ date, data }) => {
-      return {
-        date,
-        data: data.sort((a, b) => b.value - a.value)
-      };
-    });
-
-  return [keyframes, nameList];
-};
-
 function BarChartAnimation({
-  data,
   numOfBars,
-  numOfSlice,
-  duration,
   width,
   height,
-  padding
+  padding,
+  keyframes,
 }) {
-  const [keyframes, nameList] = React.useMemo(
-    () => makeKeyFrames(data, numOfSlice), 
-    [data, numOfSlice]
+  const nameList = React.useMemo(
+    () => {
+      if (keyframes.length === 0) {
+        return []
+      }
+      return keyframes[0].data.map(d => d.name);
+    }, 
+    [keyframes]
   );
   const [frameIdx, setFrameIdx] = React.useState(0);
   const frame = keyframes[frameIdx];
